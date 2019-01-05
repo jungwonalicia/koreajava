de6c6adff6e1b04e57a691a6dfc7f8e9b641a991
jungwonalicia <aliciawill@naver.com>
2019-01-05 14:49:13 +0900

    Add files via upload

--- day5/src/window2/MyAge.java
+++ day5/src/window2/MyAge.java
@@ -71,8 +71,6 @@ public class MyAge {
 			}
 		});
 		
-		
-		
 		f.setVisible(true);
 		
 	}
--- day5/src/window2/Pizza.java
+++ day5/src/window2/Pizza.java
@@ -7,8 +7,11 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Pizza {
 	JFrame f;
@@ -21,11 +24,14 @@ public class Pizza {
 	int bulgoki = 0;
 
 	JTextArea list;
+	private JLabel label;
+	private JLabel lblNewLabel;
+	private JButton btnNewButton;
 
 	public Pizza() {
 		// 1. 부품 복사(객체 생성)
 		f = new JFrame();
-		f.setSize(350, 200);
+		f.setSize(354, 222);
 
 		top = new JLabel("<<<<<<< 자바 피자에 오신 것을 환영합니다. 반갑습니다.. >>>>>>>>");
 		count = new JLabel("개수");
@@ -35,16 +41,34 @@ public class Pizza {
 		total = new JTextField(20);
 
 		FlowLayout flow = new FlowLayout();
-		f.setLayout(flow);
+		f.getContentPane().setLayout(flow);
 		list = new JTextArea(3, 30);
 
-		f.add(top);
-		f.add(b1);
-		f.add(b2);
-		f.add(b3);
-		f.add(count);
-		f.add(total);
-		f.add(list);
+		f.getContentPane().add(top);
+		f.getContentPane().add(b1);
+		f.getContentPane().add(b2);
+		f.getContentPane().add(b3);
+		f.getContentPane().add(count);
+		f.getContentPane().add(total);
+		f.getContentPane().add(list);
+		
+		lblNewLabel = new JLabel("총계");
+		lblNewLabel.setForeground(Color.RED);
+		f.getContentPane().add(lblNewLabel);
+		
+		btnNewButton = new JButton("나를 눌러요");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "나를 정말 누르셨군요.");
+			}
+		});
+		btnNewButton.setFont(new Font("굴림", Font.BOLD, 18));
+		btnNewButton.setBackground(Color.BLACK);
+		btnNewButton.setForeground(Color.BLUE);
+		f.getContentPane().add(btnNewButton);
+		
+		label = new JLabel("");
+		f.getContentPane().add(label);
 
 		b1.addActionListener(new ActionListener() {
 			@Override
